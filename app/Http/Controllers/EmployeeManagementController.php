11a2ef08d8ee3a75797a664f6169a3f1bbc5c8e2
rommelaniciete01 <rommel.aniciete@analytica.ph>
2026-03-25 15:36:45 +0800

    start

---
+++ app/Http/Controllers/EmployeeManagementController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\EmployeeManagement;
+use Illuminate\Http\Request;
+
+class EmployeeManagementController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(EmployeeManagement $employeeManagement)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(EmployeeManagement $employeeManagement)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, EmployeeManagement $employeeManagement)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(EmployeeManagement $employeeManagement)
+    {
+        //
+    }
+}
